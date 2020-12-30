43e0ab191e1f8ee32fb28e3e3db5465541ddafd0
Andrew Meiners <andrew.meiners@gmail.com>
2020-12-30 18:22:45 -0500

    Adding 1B and some display outputs

--- aoc-d1/Main.hs
+++ aoc-d1/Main.hs
@@ -6,5 +6,9 @@ main :: IO ()
 main = do
   [filePath] <- getArgs
   entries <- map read . lines <$> readFile filePath :: IO [Int]
+  putStrLn "1A"
   putStrLn $ show $ head [ i * j | i <- entries, j <- entries, i + j == 2020]
+  putStrLn "1B"
+  putStrLn $ show $ head [ i * j * k | i <- entries, j <- entries, k <- entries, i + j + k == 2020]
+
 
